381163c77423b107cf098fc74345da2918bdad5d
LAPTOP-CN9UEJMV\JO3JO <wayne.lin@lumina-tech.com>
2020-12-02 14:58:56 +0800

    1. update publish file

---
+++ LUMCustomizations/Graph_Extensions/POOrderEntry_Extensions.cs
@@ -0,0 +1,34 @@
+﻿using PX.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.PO
+{
+    public class POOrderEntry_Extensions : PXGraphExtension<POOrderEntry>
+    {
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Base.report.AddMenuAction(DomesticPO);
+        }
+
+        #region Action
+        public PXAction<POOrder> DomesticPO;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Domestic PO", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable domesticPO(PXAdapter adapter)
+        {
+            var _reportID = "lm613000";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            var _poOrder = adapter.Get<POOrder>().ToList().FirstOrDefault();
+            parameters["OrderNbr"] = _poOrder.OrderNbr;
+            throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+        }
+        #endregion
+    }
+}
