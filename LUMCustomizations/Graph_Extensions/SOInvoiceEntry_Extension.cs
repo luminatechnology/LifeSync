f993d4d0a33c4b9e001b371948e2c6beb216305b
Dennis <nirartha3214@gmail.com>
2020-12-15 15:59:25 +0800

    update the Invoice report

---
+++ LUMCustomizations/Graph_Extensions/SOInvoiceEntry_Extension.cs
@@ -0,0 +1,32 @@
+﻿using PX.Data;
+using PX.Objects.AR;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PX.Objects.SO
+{
+    public class SOInvoiceEntry_Extension : PXGraphExtension<SOInvoiceEntry>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Base.report.AddMenuAction(CommercialInvoiceReport);
+        }
+
+        #region Action
+        public PXAction<ARInvoice> CommercialInvoiceReport;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Commercial Invoice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable commercialInvoiceReport(PXAdapter adapter)
+        {
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters["RefNbr"] = Base.Document.Current.RefNbr;
+                throw new PXReportRequiredException(parameters, "LM602000", "Report LM602000");
+            }
+            return adapter.Get();
+        }
+        #endregion
+    }
+}
