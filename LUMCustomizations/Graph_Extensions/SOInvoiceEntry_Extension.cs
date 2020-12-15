8360188f898ed73bcc655babae706dee727e6eb2
Dennis <nirartha3214@gmail.com>
2020-12-15 17:47:58 +0800

    upload credit note report

--- LUMCustomizations/Graph_Extensions/SOInvoiceEntry_Extension.cs
+++ LUMCustomizations/Graph_Extensions/SOInvoiceEntry_Extension.cs
@@ -11,6 +11,7 @@ namespace PX.Objects.SO
         {
             base.Initialize();
             Base.report.AddMenuAction(CommercialInvoiceReport);
+            Base.report.AddMenuAction(CreditNoteReport);
         }
 
         #region Action
@@ -24,6 +25,22 @@ namespace PX.Objects.SO
                 Dictionary<string, string> parameters = new Dictionary<string, string>();
                 parameters["RefNbr"] = Base.Document.Current.RefNbr;
                 throw new PXReportRequiredException(parameters, "LM602000", "Report LM602000");
+            }
+            return adapter.Get();
+        }
+        #endregion
+
+        #region Action
+        public PXAction<ARInvoice> CreditNoteReport;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Credit Note", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable creditNoteReport(PXAdapter adapter)
+        {
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters["RefNbr"] = Base.Document.Current.RefNbr;
+                throw new PXReportRequiredException(parameters, "LM602005", "Report LM602005");
             }
             return adapter.Get();
         }
