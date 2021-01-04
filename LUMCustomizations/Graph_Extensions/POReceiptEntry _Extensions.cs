1b294f70a7b54e60d9e128dd182b20381c74ffbf
Dennis <nirartha3214@gmail.com>
2021-01-04 17:50:56 +0800

    add PO Receipt and modify report column or schema

---
+++ LUMCustomizations/Graph_Extensions/POReceiptEntry _Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using PX.Data;
+
+namespace PX.Objects.PO
+{
+    public class POReceiptEntry_Extension : PXGraphExtension<POReceiptEntry>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Base.report.AddMenuAction(POReceipt);
+        }
+
+        
+        #region Action
+        public PXAction<POOrder> POReceipt;
+        [PXButton]
+        [PXUIField(DisplayName = "Print PO Receipt", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable pOReceipt(PXAdapter adapter)
+        {
+            var _reportID = "LM646000";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters["ReceiptNbr"] = Base.Document.Current.ReceiptNbr;
+            throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+        }
+        #endregion
+    }
+}
