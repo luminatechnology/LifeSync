bf9ab1a141710602413b1fa58f432742817c4006
WayneLin <wayne.lin@lumina-tech.com>
2021-01-22 16:08:08 +0800

    1.Fix Shipment plan endCarton Number bug

--- LUMCustomizations/Graph/LumShipmentPlanMaint.cs
+++ LUMCustomizations/Graph/LumShipmentPlanMaint.cs
@@ -281,7 +281,7 @@ namespace LumCustomizations.Graph
                             break;
                     }
                 }
-                row.EndCartonNbr = (row.StartCartonNbr ?? 1) + (int)(row.PlannedShipQty % qtyCarton > 0 ? row.PlannedShipQty / qtyCarton + 1 : row.PlannedShipQty / qtyCarton);
+                row.EndCartonNbr = (row.StartCartonNbr ?? 1) + (int)Math.Ceiling((row.PlannedShipQty / qtyCarton).Value) - 1;
                 row.EndLabelNbr = (row.StartLabelNbr ?? 1) + (int)Math.Ceiling((row.PlannedShipQty / qtyCarton).Value) - 1;
                 row.CartonQty = qtyCarton == 0 ? 5000M : (decimal)e.NewValue / qtyCarton;
                 row.NetWeight = (decimal)e.NewValue * item.BaseItemWeight;
