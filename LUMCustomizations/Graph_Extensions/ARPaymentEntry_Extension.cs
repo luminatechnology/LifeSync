744e4658497b04c6f74eb114537dbacfcf535f57
WayneLin <wayne.lin@lumina-tech.com>
2021-01-27 12:31:39 +0800

    1.Update Label Report

---
+++ LUMCustomizations/Graph_Extensions/ARPaymentEntry_Extension.cs
@@ -0,0 +1,19 @@
+﻿using PX.Data;
+using PX.Objects.AR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUMCustomizations.Graph_Extensions
+{
+    public class ARPaymentEntry_Extension : PXGraphExtension<ARPaymentEntry>
+    {
+        protected virtual void _(Events.FieldUpdated<ARAdjust.curyAdjdAmt> e, PXFieldUpdated baseMethod)
+        {
+            baseMethod(e.Cache, e.Args);
+            e.Cache.SetValue<ARAdjust.adjdCuryRate>(e.Cache, (decimal?)e.NewValue / Base.Document.Current.CuryDocBal);
+        }
+    }
+}
