000fd3df9b305c4b8081ca56d46435bdc1fad563
WayneLin <wayne.lin@lumina-tech.com>
2021-01-28 21:59:25 +0800

    1.Fix AR302000 Bug

--- LUMCustomizations/Graph_Extensions/ARPaymentEntry_Extension.cs
+++ LUMCustomizations/Graph_Extensions/ARPaymentEntry_Extension.cs
@@ -29,12 +29,45 @@ namespace PX.Objects.AR
             PXUIFieldAttribute.SetVisible<ARAdjust.curyAdjdAmt>(e.Cache, null, library.GetCrossRateOverride);
         }
 
-        protected virtual void _(Events.FieldUpdated<ARAdjust.curyAdjgAmt> e, PXFieldUpdated invokeMethod)
+        //protected virtual void _(Events.FieldUpdated<ARAdjust.curyAdjdAmt> e, PXFieldUpdated invokeMethod)
+        //{
+        //    invokeMethod(e.Cache, e.Args);
+        //    // ReCalc usrRemCuryAdjdAmt
+        //    var _RefBalance = e.Cache.GetExtension<ARAdjustExtension>(e.Row).UsrBaseBalance;
+        //    // var _RefBalance = e.Cache.GetValueExt<ARAdjustExtension.usrBaseBalance>(e.Row);
+        //    var _curyAdjdAmt = e.Cache.GetValue<ARAdjust.curyAdjdAmt>(e.Row);
+        //    e.Cache.SetValueExt<ARAdjustExtension.usrRemCuryAdjdAmt>(e.Row, _RefBalance - (decimal?)_curyAdjdAmt);
+        //}
+
+        protected virtual void _(Events.FieldSelecting<ARAdjustExtension.usrRemCuryAdjdAmt> e)
         {
-            invokeMethod(e.Cache, e.Args);
-            // ReCalc usrRemCuryAdjdAmt
-            e.Cache.SetValue<ARAdjustExtension.usrRemCuryAdjdAmt>(e.Row,
-               (decimal?)e.Cache.GetValue<ARAdjustExtension.usrBaseBalance>(e.Row) - (decimal?)e.Cache.GetValue<ARAdjust.curyAdjdAmt>(e.Row));
+            if (e.Row == null)
+                return;
+            var ARInvocieCuryInfo = from t in new PXGraph().Select<ARInvoice>()
+                                    where t.RefNbr == (e.Row as ARAdjust).AdjdRefNbr
+                                    select t;
+            var _refBalance = ARInvocieCuryInfo.FirstOrDefault()?.CuryLineTotal ?? 0;
+            var _curyAdjdAmt = e.Cache.GetValue<ARAdjust.curyAdjdAmt>(e.Row) ?? 0;
+            e.ReturnValue = _refBalance - (Decimal?)_curyAdjdAmt;
+        }
+
+        //protected virtual void _(Events.FieldUpdated<ARAdjust.adjdRefNbr> e, PXFieldUpdated invokeMethod)
+        //{
+        //    invokeMethod(e.Cache, e.Args);
+        //    var _RefBalance = e.Cache.GetExtension<ARAdjustExtension>(e.Row).UsrBaseBalance;
+        //    var _curyAdjdAmt = e.Cache.GetValue<ARAdjust.curyAdjdAmt>(e.Row);
+        //    e.Cache.SetValueExt<ARAdjustExtension.usrRemCuryAdjdAmt>(e.Row, _RefBalance - (decimal?)_curyAdjdAmt);
+        //}
+
+        protected virtual void _(Events.FieldSelecting<ARAdjustExtension.usrBaseBalance> e)
+        {
+            if (e.Row == null)
+                return;
+
+            var ARInvocieCuryInfo = from t in new PXGraph().Select<ARInvoice>()
+                                    where t.RefNbr == (e.Row as ARAdjust).AdjdRefNbr
+                                    select t;
+            e.ReturnValue = ARInvocieCuryInfo.FirstOrDefault()?.CuryLineTotal;
         }
 
         #endregion
