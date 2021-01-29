e66feca8ed7bee3b0e61ce034301891ab22c4d0a
Dennis <nirartha3214@gmail.com>
2021-01-29 17:09:39 +0800

    update Inventory Receipt Report - rukudan

--- LUMCustomizations/Graph_Extensions/INReceiptEntry_Extension.cs
+++ LUMCustomizations/Graph_Extensions/INReceiptEntry_Extension.cs
@@ -10,6 +10,7 @@ namespace PX.Objects.IN
         {
             base.Initialize();
             Base.report.AddMenuAction(InventoryReceiptReport);
+            Base.report.AddMenuAction(InventoryReceiptReportruku);
         }
 
         #region Action
@@ -26,6 +27,23 @@ namespace PX.Objects.IN
                 parameters["PeriodTo"] = null;
                 parameters["PeriodFrom"] = null;
                 throw new PXReportRequiredException(parameters, "LM612000", "Report LM612000");
+            }
+            return adapter.Get();
+        }
+
+        public PXAction<INRegister> InventoryReceiptReportruku;
+        [PXButton]
+        [PXUIField(DisplayName = "Inventory Receipt Report - rukudan", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable inventoryReceiptReportruku(PXAdapter adapter)
+        {
+            if (Base.receipt.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters["DocType"] = Base.receipt.Current.DocType;
+                parameters["RefNbr"] = Base.receipt.Current.RefNbr;
+                parameters["PeriodTo"] = null;
+                parameters["PeriodFrom"] = null;
+                throw new PXReportRequiredException(parameters, "LM612010", "Report LM612010");
             }
             return adapter.Get();
         }
