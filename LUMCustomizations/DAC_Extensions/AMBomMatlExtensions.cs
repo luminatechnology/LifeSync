25070ed3721f940a33dadd01cb658efd3652ba50
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-05-21 07:28:49 +0000

    This modification is according to spec [Design Spec - Add BOM material scrap note field].

---
+++ LUMCustomizations/DAC_Extensions/AMBomMatlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using JAMS.AM;
+using PX.Data;
+using PX.Objects;
+using System;
+
+namespace JAMS.AM
+{
+    public class AMBomMatlExt : PXCacheExtension<JAMS.AM.AMBomMatl>
+    {
+        #region UsrScrapeNote
+        [PXDBDecimal(6, MinValue = 0.0)]
+        [PXDefault(TypeCode.Decimal, "0.0", PersistingCheck = PXPersistingCheck.Nothing)]
+        [PXUIField(DisplayName = "Scrape Note")]
+        public virtual Decimal? UsrScrapeNote { get; set; }
+        public abstract class usrScrapeNote : PX.Data.BQL.BqlDecimal.Field<usrScrapeNote> { }
+        #endregion
+    }
+}
